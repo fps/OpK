8289637d137b0809c47fa11e4aa72be6290f2c44
Florian Paul Schmidt <mista.tapas@gmx.net>
2018-12-14 21:48:44 +0100

    playing with trees

--- test.jl
+++ test.jl
@@ -1,11 +1,93 @@
+#function chain1(q)
+#    return (
+#        rotational([1 0 0]', [0 0 1]')(q[1]), 
+#        rotational([1 0 0]', [0 0 1]')(q[2]), 
+#        rotational([1 0 0]', [0 0 1]')(q[3]), 
+#        rotational([1 0 0]', [0 0 1]')(q[4])
+#    )
+#end
+
 function chain1(q)
-    return (
+    return [
         rotational([1 0 0]', [0 0 1]')(q[1]), 
         rotational([1 0 0]', [0 0 1]')(q[2]), 
         rotational([1 0 0]', [0 0 1]')(q[3]), 
         rotational([1 0 0]', [0 0 1]')(q[4])
+    ]
+end
+
+function tree1()
+    return [
+        rotational([1 0 0]', [0 0 1]'), 
+        rotational([1 0 0]', [0 0 1]'), 
+        [
+            rotational([1 0 0]', [0 0 1]'), 
+            rotational([1 0 0]', [0 0 1]')
+        ],
+        [
+            rotational([1 0 0]', [0 0 1]'), 
+            rotational([1 0 0]', [0 0 1]')
+        ]
+    ]
+end
+
+simple_tree1 = (
+    rotational([1 0 0]', [0 0 1]'), (
+        rotational([1 0 0]', [0 0 1]'), (
+            (
+                rotational([1 0 0]', [0 0 1]'), (
+                    rotational([1 0 0]', [0 0 1]'), ()
+                )
+            ), 
+            (
+                rotational([1 0 0]', [0 0 1]'), (
+                    rotational([1 0 0]', [0 0 1]') , ()
+                )
+            )
+        )
+    )
+)
+
+function tree2()
+    return (
+        rotational([1 0 0]', [0 0 1]'), (
+            rotational([1 0 0]', [0 0 1]'), (
+                (
+                    rotational([1 0 0]', [0 0 1]'), (
+                        rotational([1 0 0]', [0 0 1]'),  
+                    )
+                ), 
+                (
+                    rotational([1 0 0]', [0 0 1]'), (
+                        rotational([1 0 0]', [0 0 1]') , 
+                    )
+                )
+            )
+        )
     )
 end
+
+function partial_trees(a::Array{AbstractArray})
+    println("aa");
+end
+
+function partial_trees(a::AbstractArray)
+    println("a");
+end
+
+function partial_trees(a)
+    ret = [];
+    for index = 1:length(a)
+        
+    end
+end
+
+function partial_transforms(t, q)
+    
+end
+
+show(partial_transforms(tree1, [0 0 0 0 0 0]))
+println("")
 
 function partial_ranges(r)
     return map(i -> 1:i, r)
@@ -42,7 +124,7 @@ end
 
 function jacobian(c, q)
     return ForwardDiff.jacobian(
-        p -> fk(c, p),
+        p -> sum(fk(c, p)),
         q
     )
 end
