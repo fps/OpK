57778ab3bffeeaee274f61c6e3a3d736b76b8fc0
Florian Paul Schmidt <mista.tapas@gmx.net>
2018-12-15 14:19:28 +0100

    add tree_map function

--- src/tree.jl
+++ src/tree.jl
@@ -15,3 +15,18 @@ function traverse(p, t::Tuple)
         return (p * t[1], traverse(p * t[1], t[2]))
     end 
 end
+
+function tree_map(f, t)
+    return f(t)
+end
+
+function tree_map(f, t::Tuple)
+    if length(t) == 0
+        return ()
+    end
+    if t[1] isa Tuple 
+        return map(t -> tree_map(f, t), t) 
+    else 
+        return (f(t[1]), tree_map(f, t[2]))
+    end 
+end
